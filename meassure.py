394fb4b4030415c623e797a083edaf6baa13a4f2
p-adema <117938385+p-adema@users.noreply.github.com>
2024-09-16 14:47:17 +0200

    Update calibration notebook

--- meassure.py
+++ meassure.py
@@ -11,23 +11,26 @@ waitTime = 0.042
 
 ERR_MAR = 100
 
-def find_edge(m : list):
+
+def find_edge(m: list):
     edge = 0.0
     for i in range(1, len(m)):
-        if not (m[i] in range(m[i-1] - ERR_MAR, m[i-1] + ERR_MAR)):
-            edge = m[i-1]
+        if not (m[i] in range(m[i - 1] - ERR_MAR, m[i - 1] + ERR_MAR)):
+            edge = m[i - 1]
             break
-        if i == len(m)-1:
+        if i == len(m) - 1:
             print("Can't find the edge")
     return edge
 
+
 # Dist: negative for less than a meter positive for more than a meter
-def drive_dist(dist : float):
+def drive_dist(dist: float):
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
     sleep(2.3 - waitTime + dist)
     # print(arlo.go_diff(leftSpeed/2, 0, 1, 1))
     sleep(waitTime)
     arlo.stop()
+
 
 def go_right_m():
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
@@ -40,6 +43,7 @@ def go_right_m():
     edge = find_edge(m)
     return [mid_dist, edge]
 
+
 def go_left_m():
     print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
     m = []
@@ -50,5 +54,6 @@ def go_left_m():
     print(arlo.stop())
     edge = find_edge(m)
     return [mid_dist, edge]
-    
+
+
 print(go_right_m())
